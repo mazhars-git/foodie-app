f276a223caf3a3df035b970844c12e7f2a9ac4ae
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-13 23:53:30 +0600

    upadate

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "../../Css/footer.css";
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="footer">
+            <p>Copyright &copy; 2022; All rights reserved Mazharul Islam.</p>
+        </section>
     );
 };
 
