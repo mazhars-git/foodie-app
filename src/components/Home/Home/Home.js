243a482a426073b154380c95b8ca1fb2a76cacbc
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-15 23:36:07 +0600

    mui material added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutUs from '../AboutUs/AboutUs';
 import Navbar from '../Navbar/Navbar';
 import Footer from './../Footer/Footer';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Navbar />
+            <AboutUs />
             <Footer />
         </div>
     );
