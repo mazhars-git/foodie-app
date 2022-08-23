099d39ca19aa285518bbfd885f91fa2eade9d54b
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-23 09:39:42 +0600

    delete swiper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home/Home';
-import "swiper/css/bundle";
+
 
 function App() {
   return (
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,14 +2,12 @@ import React from 'react';
 import AboutUs from '../AboutUs/AboutUs';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../../Shared/Footer/Footer';
-import HeaderSlider from '../../Home/HeaderSlider/HeaderSlider';
-import "swiper/css/bundle";
+
 
 const Home = () => {
     return (
         <div>
             <Navbar />
-            <HeaderSlider />
             <AboutUs />
             <Footer />
         </div>
